d0c4e8748be2b265cba8c710fa3e2cc24de4dada
snfansclub <67875881+snfansclub@users.noreply.github.com>
2020-07-12 18:34:03 +0700

    Update extend.php

--- extend.php
+++ extend.php
@@ -9,7 +9,7 @@ return [
         ->configure(function (Configurator $config) {
              $config->BBCodes->addCustom(
                '[naver id={NUMBER1?}][/naver]',
-               'l<iframe style="height: 416px; width: 100%" src="//tv.naver.com/embed/{NUMBER1}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
+               '<iframe style="height: 416px; width: 100%" src="//tv.naver.com/embed/{NUMBER1}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
             );
         })
 ];
